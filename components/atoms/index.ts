56af442c11401621de5e4d7fa4b5da5b061bac98
Sonali Parekh <sonali.parekh912@gmail.com>
2026-01-17 15:01:16 +0530

    chore: add atomic structure entries

---
+++ components/atoms/index.ts
@@ -0,0 +1,5 @@
+export { Box, HBox, VBox } from "./box";
+export { Button, buttonVariants } from "./button";
+export { Input } from "./input";
+export { Switch } from "./switch";
+export { Text } from "./text";
