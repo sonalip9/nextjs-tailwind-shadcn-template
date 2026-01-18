95fbf1f4d98126b674a3d7c96e0e80dac9a376ae
Sonali Parekh <sonali.parekh912@gmail.com>
2026-01-18 14:37:07 +0530

    refactor: restructure exports for Box components

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,4 +1,11 @@
-export { Box, HBox, VBox } from "./box";
+export {
+  Box,
+  HBox,
+  VBox,
+  type BoxProps,
+  type HBoxProps,
+  type VBoxProps,
+} from "./box";
 export { Button, buttonVariants } from "./button";
 export { Input } from "./input";
 export { Switch } from "./switch";
