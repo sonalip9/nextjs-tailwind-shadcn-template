a7683a779e6c588118f4daa8b33ecd8260c28d97
Sonali Parekh <sonali.parekh912@gmail.com>
2026-01-18 19:14:53 +0530

    refactor: move configurations in package.json

--- lint-staged.config.mjs
+++
@@ -1,8 +0,0 @@
-const config = {
-  "*.{js,jsx,mjs}": ["eslint --fix", "prettier --write"],
-  "*.{ts,tsx}": ["eslint --fix", "prettier --write", "tsc --noEmit"],
-
-  "*.{json,md,css}": ["prettier --write"],
-};
-
-export default config;
