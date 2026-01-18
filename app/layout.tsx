b7c841ad6c42d508cbf99280bf52d004593f34ad
Sonali Parekh <sonali.parekh912@gmail.com>
2026-01-18 13:39:08 +0530

    refactor: adjust HTML structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
+      <body>
         <ThemeToggle />
         {children}
       </body>
