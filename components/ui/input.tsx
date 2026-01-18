92b98fdb46e1ce3d422c7e62436c63e462826f82
Sonali Parekh <sonali.parekh912@gmail.com>
2026-01-18 19:40:26 +0530

    chore: format all files

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<
       type={type}
       className={cn(
         "flex h-xl w-full rounded-md border border-input bg-transparent px-sm py-xs text-sm shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-        className,
+        className
       )}
       {...props}
     />
